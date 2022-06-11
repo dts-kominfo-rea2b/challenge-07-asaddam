4e27a72f438ff0747f8c8f1ca1d0cf6f381d5e76
user <user@ROBOT.local>
2022-06-11 16:59:24 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const countData = (data, findData) => {
+  return new Promise((resolve, reject) => {
+    if (findData == undefined) {
+      reject("Data undefined");
+    } else {
+      var result = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (findData == data[i].hasil) {
+          result++;
+        }
+      }
+    }
+    return resolve(result);
+  });
+};
+
+const promiseOutput = async (findData) => {
+  const countTheaterIXX = await promiseTheaterIXX()
+    .then((hasilSetelehNonton) =>
+      countData(hasilSetelehNonton, findData)
+    );
+  const countTheaterVGC = await promiseTheaterVGC()
+    .then((hasilSetelehNonton) =>
+      countData(hasilSetelehNonton, findData)
+    );
+    return countTheaterIXX + countTheaterVGC;
+};
 
 module.exports = {
   promiseOutput,
